ba23acccbaf884d0f49e6a1206b4a78b27391a20
Grzegorz <kalarus.g@gmail.com>
2019-02-22 08:36:43 +0100

    Updates

---
+++ precache-manifest.e2aff7dd5af9defd24d08086834a7948.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "ae5060d62775413c900048697414b958",
+    "url": "/shop/static/media/remove.ae5060d6.svg"
+  },
+  {
+    "revision": "863068c24a12e9c9c3b5",
+    "url": "/shop/static/css/main.7fd2788f.chunk.css"
+  },
+  {
+    "revision": "463990120ab1f51f2839",
+    "url": "/shop/static/js/runtime~main.46399012.js"
+  },
+  {
+    "revision": "db33efd7bfec16956364",
+    "url": "/shop/static/js/2.db33efd7.chunk.js"
+  },
+  {
+    "revision": "2ed164f618de58045c2e0b28a8c015bf",
+    "url": "/shop/static/media/iphone.2ed164f6.png"
+  },
+  {
+    "revision": "a0b10344f9d8e44452eb4319cd46a2cc",
+    "url": "/shop/static/media/cart.a0b10344.svg"
+  },
+  {
+    "revision": "863068c24a12e9c9c3b5",
+    "url": "/shop/static/js/main.863068c2.chunk.js"
+  },
+  {
+    "revision": "31f7cdde486760b53dee646e7c7dc139",
+    "url": "/shop/static/media/removeRed.31f7cdde.svg"
+  },
+  {
+    "revision": "ad22c368600b497b68ebcdd3f2eb43da",
+    "url": "/shop/static/media/success.ad22c368.jpeg"
+  },
+  {
+    "revision": "89a3b901dbfe283de0049fb5d8f35f7a",
+    "url": "/shop/static/media/page.89a3b901.jpg"
+  },
+  {
+    "revision": "9a8e9bec963659f1c94ffaa09b1b9d2b",
+    "url": "/shop/static/media/plus.9a8e9bec.svg"
+  },
+  {
+    "revision": "3e5a3edea1432ecf3bcec601d0d40033",
+    "url": "/shop/static/media/minus.3e5a3ede.svg"
+  },
+  {
+    "revision": "fcd012a9931fad248b3ff52e3e1b3f89",
+    "url": "/shop/index.html"
+  }
+];
